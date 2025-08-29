c6a7b9347cf57d98eabb243a7507d59230bb13fa
kssimay <radchenko.alltools@gmail.com>
2025-08-30 00:50:18 +0500

    Revert "Выгрузка всех полей в админке"
    This reverts commit 54e2aa225aa8d26ec5ad8399a42e1409cf2d11bb.

--- price_manager/core/resources.py
+++ price_manager/core/resources.py
@@ -1,35 +1,16 @@
 from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
-from .models import MainProduct, Supplier, Manufacturer, Category
+from .models import *
 
 
 class MainProductResource(resources.ModelResource):
-    supplier = fields.Field(
+    supplier_name = fields.Field(
         column_name='supplier',
         attribute='supplier',
         widget=ForeignKeyWidget(Supplier, field='name')
     )
-    manufacturer = fields.Field(
-        column_name='manufacturer',
-        attribute='manufacturer',
-        widget=ForeignKeyWidget(Manufacturer, field='name')
-    )
-    category = fields.Field(
-        column_name='category',
-        attribute='category',
-        widget=ForeignKeyWidget(Category, field='name')
-    )
-
     class Meta:
         model = MainProduct
-        fields = [f.name for f in MainProduct._meta.fields]
+        fields = [field for field in MP_FIELDS if field not in ['updated_at']]
         export_order = fields
-        import_id_fields = ['id']
-        skip_unchanged = True
-        report_skipped = True
-
-    def skip_row(self, instance, original):
-        """
-        Пропускаем строки, у которых нет ID (чтобы не создавать новые товары).
-        """
-        return instance.id is None
+        import_id_fields = ['sku']
