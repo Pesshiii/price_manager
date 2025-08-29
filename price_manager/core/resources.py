54e2aa225aa8d26ec5ad8399a42e1409cf2d11bb
kssimay <radchenko.alltools@gmail.com>
2025-08-29 09:33:10 +0500

    Выгрузка всех полей в админке

--- price_manager/core/resources.py
+++ price_manager/core/resources.py
@@ -1,16 +1,35 @@
 from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
-from .models import *
+from .models import MainProduct, Supplier, Manufacturer, Category
 
 
 class MainProductResource(resources.ModelResource):
-    supplier_name = fields.Field(
+    supplier = fields.Field(
         column_name='supplier',
         attribute='supplier',
         widget=ForeignKeyWidget(Supplier, field='name')
     )
+    manufacturer = fields.Field(
+        column_name='manufacturer',
+        attribute='manufacturer',
+        widget=ForeignKeyWidget(Manufacturer, field='name')
+    )
+    category = fields.Field(
+        column_name='category',
+        attribute='category',
+        widget=ForeignKeyWidget(Category, field='name')
+    )
+
     class Meta:
         model = MainProduct
-        fields = [field for field in MP_FIELDS if field not in ['updated_at']]
+        fields = [f.name for f in MainProduct._meta.fields]
         export_order = fields
-        import_id_fields = ['sku']
+        import_id_fields = ['id']
+        skip_unchanged = True
+        report_skipped = True
+
+    def skip_row(self, instance, original):
+        """
+        Пропускаем строки, у которых нет ID (чтобы не создавать новые товары).
+        """
+        return instance.id is None
