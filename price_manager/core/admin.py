596fd5be894fc5e850153b4956c94ff1396f6386
kssimay <radchenko.alltools@gmail.com>
2025-09-14 20:22:42 +0500

    Категории отображаются в админке

--- price_manager/core/admin.py
+++ price_manager/core/admin.py
@@ -1,6 +1,5 @@
-from import_export.admin import ImportExportModelAdmin
-from django.contrib import admin
-# from .forms import PriceManagerAdminForm
+from django.utils.safestring import mark_safe
+from core.models import Category
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from import_export.formats.base_formats import CSV
@@ -82,3 +81,27 @@ class DiscountAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'supplier']
     list_filter = ['supplier']
     search_fields = ['name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("indented_name", "parent")
+    list_display_links = ("indented_name",)
+    search_fields = ("name",)
+    list_filter = ("parent",)
+    ordering = ("parent__id", "name")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("parent")
+
+    def indented_name(self, obj):
+        # считаем глубину по parent (у вас иерархия через FK на себя)
+        depth = 0
+        p = obj.parent
+        while p:
+            depth += 1
+            p = p.parent
+        indent = "&nbsp;" * 4 * depth + ("— " if depth else "")
+        return mark_safe(f"{indent}{obj.name}")
+    indented_name.short_description = "Категория"
