4076130aa58d1601d53806ba9ea3f6f7b729f4a3
Pesshiii <pesshiii@gmail.com>
2025-10-20 14:02:20 +0300

    Полностью убрал логику корзины убрал прослойку товар-корзина(AlternateProduct)

--- price_manager/core/models.py
+++ price_manager/core/models.py
@@ -462,20 +462,6 @@ class FileModel(models.Model):
 
 # Обработка заявок
 
-class AlternateProduct(models.Model):
-  name = models.CharField(verbose_name='Название',
-                          null=False)
-  # filter = models.JSONField(verbose_name='Фильтр',
-  #                           null=True)
-  main_product = models.ForeignKey(MainProduct, 
-                                    verbose_name='Главный продукт',
-                                    related_name='alternate_products',
-                                    on_delete=models.SET_NULL,
-                                    null=True,
-                                    blank=True)
-  class Meta:
-    constraints = [models.UniqueConstraint(fields=['name', 'main_product'], name='name_main_product_constraint')]
-
 class ShopingTab(models.Model):
   user = models.ForeignKey('auth.User',
                            verbose_name='Пользователь',
@@ -483,9 +469,11 @@ class ShopingTab(models.Model):
                            related_name='shoping_tabs')
   name = models.CharField(verbose_name='Название',
                           null=False)
-  products = models.ManyToManyField(AlternateProduct,
+  products = models.ManyToManyField(MainProduct,
                                     verbose_name='Товары',
                                     related_name='shoping_tabs')
+  quantity = models.IntegerField(verbose_name='Количество',
+                                 default=1)
   open = models.BooleanField(verbose_name='Открыта',
                              default=True)
   class Meta:
--- price_manager/price_manager/urls.py
+++ price_manager/price_manager/urls.py
@@ -53,12 +53,4 @@ urlpatterns = [
     path('main-product/sync/', views.sync_main_products, name='main-product-sync'),
     path('upload/<str:name>/<int:id>/', views.FileUpload.as_view(), name='upload'),
 
-    path('shopping-tabs/', views.ShoppingTabListView.as_view(), name='shopping-tab-list'),
-    path('shopping-tabs/<int:pk>/', views.ShoppingTabDetailView.as_view(), name='shopping-tab-detail'),
-    path('shopping-tabs/<int:pk>/delete/', views.ShoppingTabDeleteView.as_view(), name='shopping-tab-delete'),
-    path('shopping-tabs/<int:tab_pk>/products/create/', views.ShoppingTabProductCreateView.as_view(), name='shopping-tab-product-create'),
-    path('shopping-tabs/<int:tab_pk>/products/<int:product_pk>/edit/', views.ShoppingTabProductUpdateView.as_view(), name='shopping-tab-product-update'),
-    path('shopping-tabs/<int:tab_pk>/products/<int:pk>/delete/', views.ShoppingTabProductDeleteView.as_view(), name='shopping-tab-product-delete'),
-    path('shopping-tabs/select/<int:product_id>/', views.ShoppingTabSelectionView.as_view(), name='shopping-tab-select'),
-    path('shopping-tabs/<int:tab_pk>/add/<int:product_id>/', views.ShoppingTabAddProductView.as_view(), name='shopping-tab-add-product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
