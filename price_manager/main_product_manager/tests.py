cadb26432cbf1c837d5fc1352120f184c22cb3c5
kssimay <radchenko.alltools@gmail.com>
2026-02-10 21:47:25 +0500

    Improve manufacturer resolution during main price import

--- price_manager/main_product_manager/tests.py
+++ price_manager/main_product_manager/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
-# Create your tests here.
+from supplier_manager.models import Manufacturer, ManufacturerDict
+from .resources import ManufacturerWidget
+
+
+class ManufacturerWidgetTests(TestCase):
+    def setUp(self):
+        self.widget = ManufacturerWidget(Manufacturer, "name")
+
+    def test_clean_uses_existing_manufacturer_case_insensitive(self):
+        manufacturer = Manufacturer.objects.create(name="Bosch")
+
+        result = self.widget.clean("bosch")
+
+        self.assertEqual(result, manufacturer)
+        self.assertEqual(Manufacturer.objects.count(), 1)
+
+    def test_clean_uses_dictionary_mapping(self):
+        manufacturer = Manufacturer.objects.create(name="DeWALT")
+        ManufacturerDict.objects.create(name="Dewalt tools", manufacturer=manufacturer)
+
+        result = self.widget.clean("dewalt tools")
+
+        self.assertEqual(result, manufacturer)
+        self.assertEqual(Manufacturer.objects.count(), 1)
+
+    def test_clean_autobinds_close_name_to_existing_manufacturer(self):
+        manufacturer = Manufacturer.objects.create(name="Makita")
+
+        result = self.widget.clean("Makitta")
+
+        self.assertEqual(result, manufacturer)
+        self.assertTrue(
+            ManufacturerDict.objects.filter(name="Makitta", manufacturer=manufacturer).exists()
+        )
+        self.assertEqual(Manufacturer.objects.count(), 1)
+
+    def test_clean_creates_new_manufacturer_when_no_match(self):
+        result = self.widget.clean("Completely New Brand")
+
+        self.assertEqual(result.name, "Completely New Brand")
+        self.assertEqual(Manufacturer.objects.count(), 1)
