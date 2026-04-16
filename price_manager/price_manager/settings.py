4abcd3089ae6653d9c1b431d09bc0209496c7fa3
Pesshiii <pesshiii@gmail.com>
2026-04-16 12:41:14 +0300

    26.04(F)Добавил поддержку S3 bucket

--- price_manager/price_manager/settings.py
+++ price_manager/price_manager/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
     'crispy_forms',
     'mptt',
+    'storages',
 
     'core',
     'file_manager',
@@ -234,6 +235,16 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
+AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "auto")
+AWS_QUERYSTRING_AUTH = True
+AWS_DEFAULT_ACL = None
+
 
 
 # Default primary key field type
