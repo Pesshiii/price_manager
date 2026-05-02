643a628e0ec307eb6e9060b5e202679569b89db1
kssimay <radchenko.alltools@gmail.com>
2026-05-02 12:24:55 +0300

    Add SelectFile upload modal view and endpoint

--- price_manager/dataframe/forms.py
+++ price_manager/dataframe/forms.py
@@ -3,3 +3,9 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Field, HTML
 
 from .models import Dataframe, FileModel
+
+
+class FileForm(forms.ModelForm):
+    class Meta:
+        model = FileModel
+        fields = ("file",)
--- price_manager/dataframe/urls.py
+++ price_manager/dataframe/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
 
-from django.urls import include, path
+from .views import fileupload as fm_views
 
 app_name = 'dataframe'
 
 urlpatterns = [
+    path('files/select/', fm_views.SelectFile.as_view(), name='fileselect'),
 ]
