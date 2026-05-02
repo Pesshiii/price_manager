6e681b688e73f091ced76af7c1364f8118a592c4
Pesshiii <pesshiii@gmail.com>
2026-05-02 18:26:53 +0300

    Удалил views.py файл в dataframe

--- price_manager/dataframe/views/__init__.py
+++ price_manager/dataframe/views/__init__.py
@@ -1,25 +1,2 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
-
-from ..forms import DataframeForm
-from ..models import Dataframe
 from .fileupload import SelectFile, FileMetaView
-
-
-class DataframeCreateView(CreateView):
-    model = Dataframe
-    form_class = DataframeForm
-    template_name = 'dataframe/create.html'
-    success_url = reverse_lazy('dataframe:create')
-
-
-class DataframeUpdateView(UpdateView):
-    model = Dataframe
-    form_class = DataframeForm
-    template_name = 'dataframe/update.html'
-    success_url = reverse_lazy('dataframe:create')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['initial_pk'] = self.object.file_id
-        return context
+from .dataframe import DataframeCreateView, DataframeUpdateView
