0e4f1b392a46429213c8042a748a805a38523867
Pesshiii <pesshiii@gmail.com>
2026-05-03 11:45:14 +0300

    Update urls.py

--- price_manager/dataframe/urls.py
+++ price_manager/dataframe/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
 from .views import (
-    FileList, FileItem, FileCreate,
-    DataframeCreateView, DataframeUpdateView, DataframeTableView)
+    FileList, FileItem, FileCreate)
 
 app_name = 'dataframe'
 
