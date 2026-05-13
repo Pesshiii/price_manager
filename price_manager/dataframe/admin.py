ef8d84cdc93d321f045ed849e0e002c6513c549c
Pesshiii <pesshiii@gmail.com>
2026-05-13 10:42:06 +0300

    Update admin.py

--- price_manager/dataframe/admin.py
+++ price_manager/dataframe/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from dataframe.models import FileModel
 
-# Register your models here.
+@admin.register(FileModel)
+class FileModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "file")
