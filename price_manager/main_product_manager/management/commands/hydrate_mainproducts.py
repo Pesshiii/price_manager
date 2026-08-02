5fd8f086650a62ca8c37eb12fb0b905c5e7d1935
Pesshiii <pesshiii@gmail.com>
2026-08-02 20:35:18 +0300

    Update hydrate_mainproducts.py

--- price_manager/main_product_manager/management/commands/hydrate_mainproducts.py
+++ price_manager/main_product_manager/management/commands/hydrate_mainproducts.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
-from ...models import MainProduct
-from ....supplier_manager.models import Supplier
-from ...pim_api import site, UpsertAsync, Job, EntityList, Where
+from main_product_manager.models import MainProduct
+from supplier_manager.models import Supplier
+from main_product_manager.pim_api import site, UpsertAsync, Job, EntityList, Where
 from time import sleep
 
 class Command(BaseCommand):
@@ -10,23 +10,36 @@ class Command(BaseCommand):
     def handle(self, *args, **options)->None:
         suppliers = Supplier.objects.all()
         s_payload = []
+        count = 0
+        def _wait_job(id:str)->str:
+                    status = site.get(Job(id=id))['status']
+                    while not status == 'success':
+                        sleep(5)
+                        status = site.get(Job(id=id))['status']
+                        if status == 'Failed':
+                            raise(RuntimeError('Migrations failed. ', ' Id: ', id, ' Message:', site.get(Job(id=id))['message']))
+                    return status
         for supplier in suppliers:
             s_payload.append({'entity':'Account', 'payload':{'name':supplier.name, 'role': 'supplier'}})
-        def _wait_job(id:str)->str:
-            status = site.get(Job(id=id))['status']
-            while not status == 'success':
-                sleep(5)
-                status = site.get(Job(id=id))['status']
-                if status == 'Failed':
-                    raise(RuntimeError('Migrations failed. ', ' Id: ', id, ' Message:', site.get(Job(id=id))['message']))
-            return status
-        s_id = site.get(UpsertAsync(payload=s_payload))['jobId']
-        _wait_job(s_id)
+            count += 1
+            if count%100==0:
+                s_id = site.get(UpsertAsync(payload=s_payload))['jobId']
+                self.stdout.write(msg=f'Задача {s_id} завершена со статусом: {_wait_job(s_id)}')
+        if not count%100==0:
+                s_id = site.get(UpsertAsync(payload=s_payload))['jobId']
+                self.stdout.write(msg=f'Задача {s_id} завершена со статусом: {_wait_job(s_id)}')
+        count = 0
         mp_payload = []
         def _get_supplier_id(name: str)->str:
             return site.get(EntityList(name='Account', where=[Where(attribute='name', type='like', value=name)]))['list'][0]['id']
         products = MainProduct.objects.all()
         for product in products:
             mp_payload.append({'entity':'Product', 'payload':{'name':product.name, 'mpn': product.article, 'number': product.article, 'defaultSupplierId':_get_supplier_id(product.supplier.name)}})
-        mp_id = site.get(UpsertAsync(payload=mp_payload))['jobId']
-        self.stdout.write(msg=_wait_job(mp_id))
+            count += 1
+            if count%100==0:
+                mp_id = site.get(UpsertAsync(payload=mp_payload))['jobId']
+                self.stdout.write(msg=f'Задача {mp_id} завершена со статусом: {_wait_job(mp_id)}')
+        if count%100==0:
+                mp_id = site.get(UpsertAsync(payload=mp_payload))['jobId']
+                self.stdout.write(msg=f'Задача {mp_id} завершена со статусом: {_wait_job(mp_id)}')
+        self.stdout.write(self.style.SUCCESS('Выполнение завершено'))
