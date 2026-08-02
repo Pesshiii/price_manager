8a8c2fa17c13be3a8952cb6fbddc0bd76fa3abb8
Pesshiii <pesshiii@gmail.com>
2026-08-02 23:08:08 +0300

    Update hydrate_mainproducts.py

--- price_manager/main_product_manager/management/commands/hydrate_mainproducts.py
+++ price_manager/main_product_manager/management/commands/hydrate_mainproducts.py
@@ -16,11 +16,11 @@ class Command(BaseCommand):
         for supplier in suppliers:
             s_payload.append({'entity':'Account', 'payload':{'name':supplier.name, 'role': 'supplier'}})
             count += 1
-            if count%2000==0:
+            if count%200==0:
                 s_id = site.get(UpsertAsync(payload=s_payload))['jobId']
                 self.stdout.write(msg=f'Задача {s_id}. Кол-во поставщиков {len(s_payload)}')
                 s_payload = []
-        if not count%2000==0:
+        if not count%200==0:
                 s_id = site.get(UpsertAsync(payload=s_payload))['jobId']
                 self.stdout.write(msg=f'Задача {s_id}. Кол-во поставщиков {len(s_payload)}')
         count = 0
@@ -30,11 +30,11 @@ class Command(BaseCommand):
         for product in products:
             mp_payload.append({'entity':'Product', 'payload':{'name':product.name, 'mpn': product.article, 'number': product.article, 'defaultSupplierId':_get_supplier_id(product.supplier.name)}})
             count += 1
-            if count%2000==0:
+            if count%200==0:
                 mp_id = site.get(UpsertAsync(payload=mp_payload))['jobId']
                 self.stdout.write(msg=f'Задача {mp_id}. Кол-во поставщиков {len(mp_payload)}')
                 mp_payload = []
-        if count%2000==0:
+        if count%200==0:
             mp_id = site.get(UpsertAsync(payload=mp_payload))['jobId']
             self.stdout.write(msg=f'Задача {mp_id}. Кол-во поставщиков {len(mp_payload)}')
         self.stdout.write(self.style.SUCCESS('Выполнение завершено'))
