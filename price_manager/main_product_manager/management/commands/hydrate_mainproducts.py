4a99bf6c28946357010af65e1a34d80d6c395663
Pesshiii <pesshiii@gmail.com>
2026-08-05 18:38:10 +0300

    Ticket: #173432. Добавлена двусторонняя связь между Pim системой и PriceManager

--- price_manager/main_product_manager/management/commands/hydrate_mainproducts.py
+++ price_manager/main_product_manager/management/commands/hydrate_mainproducts.py
@@ -2,14 +2,15 @@ from django.core.management.base import BaseCommand
 from main_product_manager.models import MainProduct
 from supplier_manager.models import Supplier
 from main_product_manager.pim_api import site, UpsertAsync, Job, EntityList, Where
+from typing import Dict, List
 from time import sleep
 
 class Command(BaseCommand):
-    help = "Вызовите эту команду чтобы насытить продукты информацией из API."
+    help = "Вызовите эту команду чтобы насытить создать связь между Pim и PriceManager"
 
     def handle(self, *args, **options)->None:
         suppliers = Supplier.objects.all()
-        products = MainProduct.objects.all().select_related('supplier')
+        products = MainProduct.objects.all().filter(pim_id__isnull=True).select_related('supplier', 'manufacturer')
         self.stdout.write(self.style.SUCCESS(f'Начало выполнения задачи. Число поставщиков: {suppliers.count()}, товаров: {products.count()}'))
         s_payload = []
         count = 0
@@ -25,21 +26,35 @@ class Command(BaseCommand):
             self.stdout.write(msg=f'Задача {s_id}. Кол-во поставщиков {len(s_payload)}')
         count = 0
         mp_payload = []
-        def _get_supplier_id(name: str)->str:
-            return site.get(EntityList(name='Account', where=[Where(attribute='name', type='like', value=name)]))['list'][0]['id']
+        def _get_supplier_id(name: str)->str|None:
+            items =  site.get(EntityList(name='Account', where=[Where(attribute='name', type='like', value=name)]))['list'][0]['id']
+            return items[0]['id'] if len(items)>0 else None
+        def _get_product_id(product: MainProduct)->str|None:
+            items = site.get(
+                    EntityList(name='Product', 
+                    select=['id'], 
+                    where=[Where(attribute='priceManagerId', type='like', value=f'{product.id}')])
+                )['list']
+            return items[0]['id'] if len(items)>0 else None
         for supplier in suppliers:
             s_id = _get_supplier_id(supplier.name)
             for product in products.filter(supplier=supplier):
+                item = {
+                    'entity':'Product', 
+                    'payload':{
+                        'priceManagerId':product.id,
+                        'name':product.name, 
+                        'mpn': product.article, 
+                        'number': product.sku,
+                        'defaultSupplierId':s_id,
+                    }
+                }
+                if not product.manufacturer is None:
+                    item['payload']['brand'] = {
+                        'name': product.manufacturer.name
+                    }
                 mp_payload.append(
-                    {
-                        'entity':'Product', 
-                        'payload':{
-                            'name':product.name, 
-                            'mpn': product.article, 
-                            'number': product.article, 
-                            'defaultSupplierId':s_id
-                            }
-                        }
+                        item
                     )
                 count += 1
                 if count%1000==0:
@@ -49,4 +64,8 @@ class Command(BaseCommand):
         if count%1000==0:
             mp_id = site.get(UpsertAsync(payload=mp_payload))['jobId']
             self.stdout.write(msg=f'Задача {mp_id}. Кол-во товаров {len(mp_payload)}')
+        self.stdout.write(self.style.SUCCESS('Создание связей'))
+        for product in products:
+            product.pim_id = _get_product_id(product)
+        MainProduct.objects.bulk_update(products, fields=['pim_id'])
         self.stdout.write(self.style.SUCCESS('Выполнение завершено'))
--- price_manager/main_product_manager/models.py
+++ price_manager/main_product_manager/models.py
@@ -11,6 +11,7 @@ from decimal import Decimal
    
 MP_TABLE_FIELDS = ['article', 'supplier', 'name', 'manufacturer','prime_cost', 'stock']
 MP_PRICES = ['prime_cost', 'wholesale_price', 'basic_price', 'm_price', 'wholesale_price_extra', 'discount_price']
+MP_IMPORTABLES = ['main_photo_url']
 PRICE_TYPES = {
   None : 'Не указано',
   'fixed_price': 'Фиксированная цена',
@@ -38,146 +39,150 @@ class MainProduct(models.Model):
     indexes = [
       GinIndex(fields=['search_vector']),
     ]
-  sku = models.CharField(verbose_name='Артикул товара',
+    pim_id = models.CharField(verbose_name='Id для системы Pim',
+                              null=True,
+                              blank=True,
+                              unique=True)
+    sku = models.CharField(verbose_name='Артикул товара',
                          null=True,
                          blank=True,
                          unique=False)
-  supplier=models.ForeignKey(Supplier,
+    supplier=models.ForeignKey(Supplier,
                              verbose_name='Поставщик',
                              related_name='main_products',
                              on_delete=models.PROTECT,
                              null=False,
                              blank=False)
-  article = models.CharField(verbose_name='Артикул поставщика',
+    article = models.CharField(verbose_name='Артикул поставщика',
                              null=False,
                              blank=False)
-  name = models.CharField(verbose_name='Название',
+    name = models.CharField(verbose_name='Название',
                           null=False,
                           blank=False)
-  category = models.ForeignKey(Category,
+    category = models.ForeignKey(Category,
                                on_delete=models.SET_NULL,
                                verbose_name='Категория',
                                related_name='mainproducts',
                                null=True,
                                blank=True)
-  manufacturer = models.ForeignKey(Manufacturer,
+    manufacturer = models.ForeignKey(Manufacturer,
                                    verbose_name='Производитель',
                                    related_name='mp_manufacturer_ptr',
                                    on_delete=models.SET_NULL,
                                    null=True,
                                    blank=True)
-  stock = models.PositiveIntegerField(verbose_name='Остаток',
+    stock = models.PositiveIntegerField(verbose_name='Остаток',
                                       null=True)
-  weight = models.DecimalField(
-      verbose_name='Вес',
-      decimal_places=1,
-      max_digits=8,
-      null=True)
-  prime_cost = models.DecimalField(
-      verbose_name='Себестоимость',
-      decimal_places=2,
-      max_digits=20,
-      null=True)
-  wholesale_price = models.DecimalField(
-      verbose_name='Оптовая цена',
-      decimal_places=2,
-      max_digits=20,
-      null=True)
-  basic_price = models.DecimalField(
-      verbose_name='Базовая цена',
-      decimal_places=2,
-      max_digits=20,
-      null=True)
-  m_price = models.DecimalField(
-      verbose_name='Цена ИМ',
-      decimal_places=2,
-      max_digits=20,
-      null=True)
-  wholesale_price_extra = models.DecimalField(
-      verbose_name='Оптовая цена доп.',
-      decimal_places=2,
-      max_digits=20,
-      null=True)
-  discount_price = models.DecimalField(
-      verbose_name='Цена со скидкой',
-      decimal_places=2,
-      max_digits=20,
-      null=True)
-  length = models.DecimalField(verbose_name='Длина',
-                               max_digits=10,
-                               decimal_places=2,
-                               default=Decimal(0))
-  width = models.DecimalField(verbose_name='Ширина',
-                               max_digits=10,
-                               decimal_places=2,
-                               default=Decimal(0))
-  depth = models.DecimalField(verbose_name='Глубина',
-                               max_digits=10,
-                               decimal_places=2,
-                               default=Decimal(0))
+    weight = models.DecimalField(
+        verbose_name='Вес',
+        decimal_places=1,
+        max_digits=8,
+        null=True)
+    prime_cost = models.DecimalField(
+        verbose_name='Себестоимость',
+        decimal_places=2,
+        max_digits=20,
+        null=True)
+    wholesale_price = models.DecimalField(
+        verbose_name='Оптовая цена',
+        decimal_places=2,
+        max_digits=20,
+        null=True)
+    basic_price = models.DecimalField(
+        verbose_name='Базовая цена',
+        decimal_places=2,
+        max_digits=20,
+        null=True)
+    m_price = models.DecimalField(
+        verbose_name='Цена ИМ',
+        decimal_places=2,
+        max_digits=20,
+        null=True)
+    wholesale_price_extra = models.DecimalField(
+        verbose_name='Оптовая цена доп.',
+        decimal_places=2,
+        max_digits=20,
+        null=True)
+    discount_price = models.DecimalField(
+        verbose_name='Цена со скидкой',
+        decimal_places=2,
+        max_digits=20,
+        null=True)
+    length = models.DecimalField(verbose_name='Длина',
+                                max_digits=10,
+                                decimal_places=2,
+                                default=Decimal(0))
+    width = models.DecimalField(verbose_name='Ширина',
+                                 max_digits=10,
+                                 decimal_places=2,
+                                 default=Decimal(0))
+    depth = models.DecimalField(verbose_name='Глубина',
+                                 max_digits=10,
+                                 decimal_places=2,
+                                 default=Decimal(0))
   
-  price_updated_at = models.DateTimeField(verbose_name='Последнее обновление цены',
-                                    null=True)
-  stock_updated_at = models.DateTimeField(verbose_name='Последнее обновление остатка',
-                                    null=True)
-  search_vector = SearchVectorField(null=True, editable=False, unique=False, verbose_name="Вектор поиска")
-  description = models.TextField(
-    verbose_name="Описание",
-    null=True,
-    blank=True)
-  def __str__(self):
-    return self.sku if self.sku else 'Не указан'
-  def _build_search_text(self) -> str:
-    """Собираем строку для поиска без join-ов."""
-    parts = [
-        self.sku or "",
-        self.article or "",
-        self.name or "",
-        self.description or "",
-        ' '.join(self.category.get_ancestors(include_self=True).values_list('name', flat=True) if self.category else ""),
-        getattr(self.supplier, "name", ""),
-        getattr(self.manufacturer, "name", ""),
-    ]
-    return " ".join(parts)
-  def rebuild_search_vector(self):
-    """Обновляет search_vector без join-полей (через константу)."""
-    text = self._build_search_text()
-    MainProduct.objects.filter(pk=self.pk).update(
-        search_vector=SearchVector(Value(text), config='russian')
-    )
+    price_updated_at = models.DateTimeField(verbose_name='Последнее обновление цены',
+                                      null=True)
+    stock_updated_at = models.DateTimeField(verbose_name='Последнее обновление остатка',
+                                      null=True)
+    search_vector = SearchVectorField(null=True, editable=False, unique=False, verbose_name="Вектор поиска")
+    description = models.TextField(
+      verbose_name="Описание",
+      null=True,
+      blank=True)
+    def __str__(self)->str:
+        return f'{self.sku}' if self.sku is not None else 'Не указан'
+    def _build_search_text(self) -> str:
+        """Собираем строку для поиска без join-ов."""
+        parts = [
+            self.sku or "",
+            self.article or "",
+            self.name or "",
+            self.description or "",
+            ' '.join(self.category.get_ancestors(include_self=True).values_list('name', flat=True) if self.category else ""),
+            getattr(self.supplier, "name", ""),
+            getattr(self.manufacturer, "name", ""),
+        ]
+        return " ".join(parts)
+    def rebuild_search_vector(self):
+        """Обновляет search_vector без join-полей (через константу)."""
+        text = self._build_search_text()
+        MainProduct.objects.filter(pk=self.pk).update(
+            search_vector=SearchVector(Value(text), config='russian')
+        )
     
-  def save(self, *args, **kwargs):
-    super().save(*args, **kwargs)
-    self.rebuild_search_vector()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.rebuild_search_vector()
   
 
 class MainProductLog(models.Model):
-  update_time = models.DateTimeField(verbose_name='Дата',
+    update_time = models.DateTimeField(verbose_name='Дата',
                                    auto_now_add=True)
-  main_product = models.ForeignKey(MainProduct,
+    main_product = models.ForeignKey(MainProduct,
                                    verbose_name='Товар',
                                    on_delete=models.CASCADE, 
                                    related_name='mp_log')
-  price = models.DecimalField(
-      verbose_name='Цена',
-      decimal_places=2,
-      max_digits=20,
-      null=True)
-  price_type = models.CharField(verbose_name='Тип цены',
-                                null=True,
-                                choices=[
-                                  (None, '----'),
-                                  ('basic_price', 'Базовая цена'),
-                                  ('prime_cost', 'Себестоимость'),
-                                  ('m_price', 'Цена ИМ'),
-                                  ('wholesale_price', 'Оптовая цена'),
-                                  ('wholesale_price_extra', 'Оптовая цена1')])
-  stock = models.PositiveIntegerField(verbose_name='Остаток',
-                                      null=True)
-  class Meta:
-    verbose_name = 'Изменения Главных продуктов'
-    constraints = []
-    ordering = ['-update_time']
+    price = models.DecimalField(
+        verbose_name='Цена',
+        decimal_places=2,
+        max_digits=20,
+        null=True)
+    price_type = models.CharField(verbose_name='Тип цены',
+                                  null=True,
+                                  choices=[
+                                    (None, '----'),
+                                    ('basic_price', 'Базовая цена'),
+                                    ('prime_cost', 'Себестоимость'),
+                                    ('m_price', 'Цена ИМ'),
+                                    ('wholesale_price', 'Оптовая цена'),
+                                    ('wholesale_price_extra', 'Оптовая цена1')])
+    stock = models.PositiveIntegerField(verbose_name='Остаток',
+                                        null=True)
+    class Meta:
+        verbose_name = 'Изменения Главных продуктов'
+        constraints = []
+        ordering = ['-update_time']
 
 DUPLICATE_LOOKUPS={
         'name':{
