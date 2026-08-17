ca2b196f6cf29fe2ba1b2a1eb14006f347ef5118
Pesshiii <pesshiii@gmail.com>
2026-08-17 07:32:09 +0300

    .

--- price_manager/core/models.py
+++ price_manager/core/models.py
@@ -3,21 +3,6 @@ from django.db.models import TextChoices
 from main_product_manager.models import MainProduct
 # Обработка заявок
 
-class AlternateProduct(models.Model):
-    name = models.CharField(verbose_name='Название',
-                          null=False)
-    # filter = models.JSONField(verbose_name='Фильтр',
-    #                           null=True)
-    main_product = models.ForeignKey(
-        MainProduct, 
-        verbose_name='Главный продукт',
-        related_name='alternate_products',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True)
-    class Meta:
-        constraints = [models.UniqueConstraint(fields=['name', 'main_product'], name='name_main_product_constraint')]
-
 class CartItem(models.Model):
     user = models.ForeignKey(
         'auth.User',
