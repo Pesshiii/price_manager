e105d56b85ffbe968f79e32f314ad37cdc7aec8f
Pesshiii <pesshiii@gmail.com>
2026-08-17 07:23:46 +0300

    Update forms.py

--- price_manager/core/forms.py
+++ price_manager/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from core.models import CartItem, ShoppingTab, AlternateProduct, ShoppingTab
+from core.models import CartItem, ShoppingTab, ShoppingTab
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
 
